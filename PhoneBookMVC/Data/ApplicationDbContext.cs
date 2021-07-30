eb57a63476cc43c3d3bcd8c753e15d3b19e1f0ac
RandyScottCoffman <randyscoffman@gmail.com>
2021-07-30 13:32:40 -0400

    first push - working project, testing on heroku

---
+++ PhoneBookMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBookMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBookMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ PhoneBookMVC/Services/Interfaces/IImageService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace PhoneBookMVC.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
